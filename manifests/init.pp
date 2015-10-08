2bff080cb83a33c4048bd6f0832b676316cd81dc
Nicolas Dandrimont <nicolas@dandrimont.eu>
2015-10-08 11:54:27 +0200

    Add documentation for ::systemd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,19 @@
-# systemd-specific stuff for Software Heritage
+# == Class: systemd
+#
+# Manage systemd configurations
+#
+# === Examples
+#
+# include ::systemd
+#
+# === Authors
+#
+# Nicolas Dandrimont <nicolas@dandrimont.eu>
+#
+# === Copyright
+#
+# Copyright 2015 The Software Heritage developers
+#
 class systemd {
   exec {'systemd-daemon-reload':
     path        => '/sbin:/usr/sbin:/bin:/usr/bin',
